74b254ab29a4465456356d172c3ef8f587ed27d4
风儿继续吹 <1031364436@qq.com>
2020-10-23 23:04:23 +0800

    在ch003-vue中增加了组件和路由的知识

---
+++ ch003-vue/src/main/java/com/example/Ch003VueApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch003VueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch003VueApplication.class, args);
+	}
+
+}
